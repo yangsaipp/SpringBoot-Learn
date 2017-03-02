fac304983317e1362b46b0749488288502fa5ff1
yangsai <yangsaipp@qq.com>
2017-03-02 20:44:38 +0800

    自定义类修改zuul路由配置

--- 02-cloud-zuul/src/main/java/com/comtop/exp/gateway/zuul/Demo9ZuulApp.java
+++ 02-cloud-zuul/src/main/java/com/comtop/exp/gateway/zuul/Demo9ZuulApp.java
@@ -1,8 +1,13 @@
 package com.comtop.exp.gateway.zuul;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.filters.RouteLocator;
+import org.springframework.cloud.netflix.zuul.filters.ZuulProperties;
+import org.springframework.context.annotation.Bean;
 
 /**
  * Hello world!
@@ -10,10 +15,23 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
  */
 @SpringBootApplication
 @EnableZuulProxy
+//@EnableAutoConfiguration(exclude={SimpleRouteLocator.class})
 public class Demo9ZuulApp 
 {
     public static void main( String[] args )
     {
         SpringApplication.run(Demo9ZuulApp.class, args);
     }
+    
+    @Autowired
+	protected ZuulProperties zuulProperties;
+
+	@Autowired
+	protected ServerProperties server;
+    
+    @Bean
+	public RouteLocator routeLocator() {
+		return new MyRouteLocator(this.server.getServletPrefix(),
+				this.zuulProperties);
+	}
 }
