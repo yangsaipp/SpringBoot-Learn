853e3ce662f82b02d8411a7a93d0e2c737f8be6e
yangsai <yangsaipp@qq.com>
2017-03-03 16:38:33 +0800

    增加zuul使用config server的配置的示例

---
+++ 02-01-cloud-zuul-config-client/src/main/java/com/comtop/exp/gateway/zuul/Demo9ZuulApp.java
@@ -0,0 +1,48 @@
+package com.comtop.exp.gateway.zuul;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.filters.RouteLocator;
+import org.springframework.cloud.netflix.zuul.filters.ZuulProperties;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+@EnableZuulProxy
+// @EnableAutoConfiguration(exclude={SimpleRouteLocator.class})
+public class Demo9ZuulApp {
+	
+	static Logger log = LoggerFactory.getLogger(Demo9ZuulApp.class);
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Demo9ZuulApp.class, args);
+		// 定时5秒后触发SpringContext的RoutesRefreshedEvent
+//		Timer timer = new Timer();
+//		timer.schedule(new TimerTask() {
+//			public void run() {
+//				log.info(" ************* Publish routes refreshed event. ");
+//				SpringContextUtil.getApplicationContext().publishEvent(
+//						new RoutesRefreshedEvent((RouteLocator) SpringContextUtil.getBean("routeLocator")));
+//			}
+//		}, 5000);
+	}
+
+	@Autowired
+	protected ZuulProperties zuulProperties;
+
+	@Autowired
+	protected ServerProperties server;
+
+//	@Bean
+	// MyRouteLocator替换自动配置使用的SimpleRouteLocator
+	public RouteLocator routeLocator() {
+		return new MyRouteLocator(this.server.getServletPrefix(), this.zuulProperties);
+	}
+}
---
+++ 02-01-cloud-zuul-config-client/src/main/java/com/comtop/exp/gateway/zuul/MyRouteLocator.java
@@ -0,0 +1,66 @@
+/******************************************************************************
+* Copyright (C) 2014 ShenZhen ComTop Information Technology Co.,Ltd
+* All Rights Reserved.
+* 本软件为深圳康拓普开发研制。未经本公司正式书面同意，其他任何个人、团体不得使用、
+* 复制、修改或发布本软件.
+*****************************************************************************/
+
+package com.comtop.exp.gateway.zuul;
+
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.netflix.zuul.filters.RefreshableRouteLocator;
+import org.springframework.cloud.netflix.zuul.filters.SimpleRouteLocator;
+import org.springframework.cloud.netflix.zuul.filters.ZuulProperties;
+import org.springframework.cloud.netflix.zuul.filters.ZuulProperties.ZuulRoute;
+
+/**
+ * @author  杨赛
+ * @since   jdk1.6
+ * @version 2017年3月2日 杨赛
+ */
+public class MyRouteLocator extends SimpleRouteLocator implements RefreshableRouteLocator {
+	
+	
+	Logger log = LoggerFactory.getLogger(MyRouteLocator.class);
+	
+	/**
+	 * 构造函数
+	 * @param servletPath
+	 * @param properties
+	 */
+	public MyRouteLocator(String servletPath, ZuulProperties properties) {
+		super(servletPath, properties);
+		log.info(" ********************* my route locator init.");
+	}
+
+	@Override
+	protected Map<String, ZuulRoute> locateRoutes() {
+		log.info(" ********************* use my route locator.");
+		Map<String, ZuulRoute> zunMap = super.locateRoutes();
+		for(Map.Entry<String, ZuulRoute> entry : zunMap.entrySet()) {
+			if(entry.getKey().equals("/baidu/**")) {
+				entry.getValue().setUrl("https://www.sogou.com/");
+			}
+		}
+		return zunMap;
+	}
+	
+	/**
+	 * 
+	 * @see org.springframework.cloud.netflix.zuul.filters.RefreshableRouteLocator#refresh()
+	 */
+	@Override
+	public void refresh() {
+		log.info(" ********************* use my route refresh.");
+		Map<String, ZuulRoute> zunMap = super.locateRoutes();
+		for(Map.Entry<String, ZuulRoute> entry : zunMap.entrySet()) {
+			if(entry.getKey().equals("/baidu/**")) {
+				entry.getValue().setUrl("https://www.sogou.com/");
+			}
+		}
+	}
+
+}
