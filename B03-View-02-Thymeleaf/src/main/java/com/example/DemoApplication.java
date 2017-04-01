bae9d7acbd0bdb7587791eeea7e5a4287e5ee35c
yangsai <yangsaipp@qq.com>
2017-04-01 11:34:44 +0800

    调整

--- B03-View-02-Thymeleaf/src/main/java/com/example/DemoApplication.java
+++ B03-View-02-Thymeleaf/src/main/java/com/example/DemoApplication.java
@@ -2,8 +2,9 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,12 +15,18 @@ import com.example.handler.MyHandlerMapping;
 
 @Controller
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication extends SpringBootServletInitializer {
 	
 	@RequestMapping("/")
     @ResponseBody
     String home() {
         return "Hello World!";
+    }
+	
+	 // 重写configure方法，让Servlet容器加载时配置应用
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DemoApplication.class);
     }
 	
 	public static void main(String[] args) {
--- B03-View-02-Thymeleaf/src/main/java/com/example/HelloController.java
+++ B03-View-02-Thymeleaf/src/main/java/com/example/HelloController.java
@@ -2,16 +2,21 @@ package com.example;
 
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloController {
 	
+	@Value("${name}")
+	private String name;
+	
 	@RequestMapping("/helloHtml")
 	public String helloHtml(Map<String, Object> map) {
 		map.put("hello", "from HelloController.helloHtml");
-		System.out.println("helloHtml");
+		System.out.println(name);
+		map.put("name", name);
 		return "hello";
 	}
 }
